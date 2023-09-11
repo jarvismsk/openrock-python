e005d98a43670ec401b2dcd7f2681cfc2710ddf3
Irfan Khan <jarvis@Irfans-MacBook-Pro.local>
2023-09-11 16:10:25 +0530

    mmodified automate

--- automate_scripts.py
+++ automate_scripts.py
@@ -2,17 +2,24 @@ import os
 import time
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
-from subprocess import run
+import subprocess
 
 # Path to the directory containing user_response.txt
-directory_to_watch = os.path.join(os.path.dirname(__file__))
+directory_to_watch = os.path.dirname(__file__)
 
 class FileChangeHandler(FileSystemEventHandler):
     def on_modified(self, event):
         if event.src_path.endswith("user_response.txt"):
             print("user_response.txt has been modified. Running scripts...")
-            run(["python", "stockcode.py"])
-            run(["python", "ml.py"])
+            # Run stockcode.py and capture the output
+            stockcode_output = subprocess.run(["python", "stockcode.py"], text=True, capture_output=True)
+            print("Output of stockcode.py:")
+            print(stockcode_output.stdout)
+            
+            # Run ml.py and capture the output
+            ml_output = subprocess.run(["python", "ml.py"], text=True, capture_output=True)
+            print("Output of ml.py:")
+            print(ml_output.stdout)
             print("Scripts executed.")
 
 if __name__ == "__main__":
